24a44dd9786afddd1a86f5eff8541882440e84fa
Zahra Mohammadiyan <zaram1831@gmail.com>
2026-07-17 17:51:06 +0330

    Complete day02 Python basics

---
+++ day02/day02.py
@@ -0,0 +1,40 @@
+#practice1
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f"Hello {name}!")
+print(f"You are {age} years old.")
+
+#practice2
+First_number = int(input("Enter First number: "))
+Second_number = int(input("Enter Second number: "))
+print (f"sum = {First_number + Second_number}")
+print (f"Difference = {First_number - Second_number}")
+print (f"Product = {First_number * Second_number}")
+print (f"Division = {First_number / Second_number}")
+
+#practice3
+Length = int(input("Enter Length: "))
+Width = int(input("Enter Width: "))
+print(f"Area = {Length * Width}")
+print(f"Perimeter = {2 * Length + 2 * Width}")
+
+#practice4
+Minutes = int(input("Enter minutes: "))
+print(f"{Minutes//60} Hours and {Minutes%60} minutes")
+
+#practice5
+user_name = input("Enter name: ")
+Price = int (input ("Enter Price: "))
+Quantity = int(input("Enter Quantity: "))
+print(f"Hello {user_name}!")
+print(f"Price of each item: {Price}")
+print(f"Quantity: {Quantity}")
+print(f"Total price = {Price * Quantity}")
+
+#practice6
+first_number = int(input("Enter First number: "))
+second_number= int(input("Enter second number: "))
+third_number= int(input("Enter third number: "))
+print((first_number + second_number + third_number) / 3)
+
+
